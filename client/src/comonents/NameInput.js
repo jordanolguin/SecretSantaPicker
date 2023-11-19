3126fd83bc654ca307277fb7e2f9b62b0ca5db5c
Jordan Olguin <jdolguin62@gmail.com>
2023-11-18 17:13:12 -0700

    added button

--- client/src/comonents/NameInput.js
+++ client/src/comonents/NameInput.js
@@ -1,16 +1,22 @@
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const NameInput = ({ name, setName }) => {
   return (
-    <Form.Group controlId="formName">
-      <Form.Label>Name</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Enter name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-    </Form.Group>
+    <>
+      <Form.Group controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="outline-primary" type="submit">
+        Submit
+      </Button>
+    </>
   );
 };
 
