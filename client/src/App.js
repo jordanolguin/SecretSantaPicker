90bbb92fbcece1bda2e8cc093c985df9c1aad8d5
Jordan Olguin <jdolguin62@gmail.com>
2023-11-18 20:51:26 -0700

    updated styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import RandomButton from "./components/RandomButton";
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <h1>Secret Santa</h1>
       <RandomButton />
     </>
   );
