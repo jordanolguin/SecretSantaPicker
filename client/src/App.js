54102bcd4d09f87708bfd7a1308c4e94113fc59b
Jordan Olguin <jdolguin62@gmail.com>
2023-11-19 19:57:15 -0700

    modularized the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
-import RandomButton from "./components/RandomButton";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import ParticipantsList from "./components/ParticipantsList";
+import EnterPin from "./components/EnterPin";
+
+const App = () => {
+  const participants = [
+    { id: 1, name: "John", pin: "1234" },
+    { id: 2, name: "Jane", pin: "5678" },
+    { id: 3, name: "Paul", pin: "9876" },
+    { id: 4, name: "Mary", pin: "4321" },
+    { id: 5, name: "Max", pin: "8765" },
+    { id: 6, name: "Sue", pin: "1357" },
+  ];
+
+  const exclusions = {
+    John: ["Jane"],
+    Jane: ["John"],
+  };
+
   return (
-    <>
-      <RandomButton />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<ParticipantsList participants={participants} />}
+        />
+        <Route
+          path="/enter-pin/:id"
+          element={
+            <EnterPin participants={participants} exclusions={exclusions} />
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
