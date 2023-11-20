42edf1ac8fcb3734b8bb9bfdb11029e26e25a716
Jordan Olguin <jdolguin62@gmail.com>
2023-11-19 20:28:56 -0700

    updated logic. Still has bugs

--- client/src/components/EnterPin.js
+++ client/src/components/EnterPin.js
@@ -1,6 +1,29 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Button, Form } from "react-bootstrap";
+import { Container, Button, Form, Modal } from "react-bootstrap";
+
+// Function to shuffle an array using Fisher-Yates algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Function to assign Secret Santas
+function assignSecretSantas(people) {
+  const shuffledPeople = [...people];
+  shuffleArray(shuffledPeople);
+
+  const assignments = {};
+  for (let i = 0; i < shuffledPeople.length; i++) {
+    const currentPerson = shuffledPeople[i];
+    const nextPerson = shuffledPeople[(i + 1) % shuffledPeople.length];
+    assignments[currentPerson.name] = nextPerson.name;
+  }
+
+  return assignments;
+}
 
 const EnterPin = ({ participants, exclusions }) => {
   const { id } = useParams();
@@ -8,6 +31,8 @@ const EnterPin = ({ participants, exclusions }) => {
 
   const [userPins, setUserPins] = useState(["", "", "", ""]);
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [assignedSanta, setAssignedSanta] = useState("");
 
   const handleAssignSanta = () => {
     setLoading(true);
@@ -15,7 +40,6 @@ const EnterPin = ({ participants, exclusions }) => {
     const enteredPin = userPins.join("");
 
     if (enteredPin.trim() === "") {
-      alert("Please enter your pin.");
       setLoading(false);
       return;
     }
@@ -28,21 +52,13 @@ const EnterPin = ({ participants, exclusions }) => {
       );
 
       if (remainingParticipants.length > 0) {
-        // Fisher-Yates shuffle
-        const shuffledParticipants = remainingParticipants.slice();
-        for (let i = shuffledParticipants.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [shuffledParticipants[i], shuffledParticipants[j]] = [
-            shuffledParticipants[j],
-            shuffledParticipants[i],
-          ];
-        }
-
-        const assignedSanta = shuffledParticipants[0].name;
-        alert(`${participant.name}, your assigned Santa is ${assignedSanta}.`);
-        setLoading(false);
+        const secretSantaAssignments = assignSecretSantas(
+          remainingParticipants
+        );
+        const assignedSanta = secretSantaAssignments[participant.name];
+        setAssignedSanta(assignedSanta);
+        setShowModal(true);
       } else {
-        alert("Uh oh! No eligible Secret Santas available.");
         setLoading(false);
       }
     }
@@ -52,6 +68,10 @@ const EnterPin = ({ participants, exclusions }) => {
     const newPins = [...userPins];
     newPins[index] = value;
     setUserPins(newPins);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   return (
@@ -73,13 +93,13 @@ const EnterPin = ({ participants, exclusions }) => {
                 max="9"
                 value={pin}
                 onChange={(e) => handlePinInputChange(index, e.target.value)}
-                style={{ width: "25%", marginRight: "10px" }}
+                style={{ width: "25%", marginRight: "0.75em" }}
               />
             ))}
           </div>
         </Form.Group>
         <Button
-          variant="outline-success"
+          variant="outline-danger"
           onClick={handleAssignSanta}
           className="mt-3"
           disabled={loading}
@@ -87,6 +107,22 @@ const EnterPin = ({ participants, exclusions }) => {
           {loading ? "Assigning Secret Santa..." : "Assign Secret Santa"}
         </Button>
       </Form>
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        style={{ textAlign: "center" }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Assigned Secret Santa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {assignedSanta && (
+            <p>
+              {participant.name}, your assigned Secret Santa is {assignedSanta}.
+            </p>
+          )}
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 };
