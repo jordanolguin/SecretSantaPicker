f889ec1b1b3a36025514676c700a69c8d92fd0b2
Jordan Olguin <jdolguin62@gmail.com>
2023-11-20 16:23:25 -0700

    updated Title

--- client/src/components/ParticipantsList.js
+++ client/src/components/ParticipantsList.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 const ParticipantsList = ({ participants }) => {
   return (
     <Container className="mt-5 d-flex align-items-center text-center justify-content-center">
-      <h2>Participant List</h2>
+      <h2>Santa's Nice List</h2>
       <div>
         {participants.map((participant) => (
           <Link
