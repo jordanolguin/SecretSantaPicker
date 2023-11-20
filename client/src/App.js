a7dd83b3397cf611a545a14bad61599230d3cbb7
Jordan Olguin <jdolguin62@gmail.com>
2023-11-19 19:59:59 -0700

    updated number input styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import ParticipantsList from "./components/ParticipantsList";
 import EnterPin from "./components/EnterPin";
 
--- client/src/components/EnterPin.js
+++ client/src/components/EnterPin.js
@@ -6,13 +6,15 @@ const EnterPin = ({ participants, exclusions }) => {
   const { id } = useParams();
   const participant = participants.find((p) => p.id.toString() === id);
 
-  const [userPin, setUserPin] = useState("");
+  const [userPins, setUserPins] = useState(["", "", "", ""]);
   const [loading, setLoading] = useState(false);
 
   const handleAssignSanta = () => {
     setLoading(true);
 
-    if (userPin.trim() === "") {
+    const enteredPin = userPins.join("");
+
+    if (enteredPin.trim() === "") {
       alert("Please enter your pin.");
       setLoading(false);
       return;
@@ -45,6 +47,13 @@ const EnterPin = ({ participants, exclusions }) => {
       }
     }
   };
+
+  const handlePinInputChange = (index, value) => {
+    const newPins = [...userPins];
+    newPins[index] = value;
+    setUserPins(newPins);
+  };
+
   return (
     <Container className="mt-5 d-flex align-items-center text-center justify-content-center">
       <h2>Enter Pin</h2>
@@ -54,14 +63,20 @@ const EnterPin = ({ participants, exclusions }) => {
       <Form>
         <Form.Group controlId="formUserPin">
           <Form.Label>Enter your 4-digit pin</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="0"
-            min="0"
-            max="9"
-            value={userPin}
-            onChange={(e) => setUserPin(e.target.value)}
-          />
+          <div style={{ display: "flex" }}>
+            {userPins.map((pin, index) => (
+              <Form.Control
+                key={index}
+                type="number"
+                placeholder="0"
+                min="0"
+                max="9"
+                value={pin}
+                onChange={(e) => handlePinInputChange(index, e.target.value)}
+                style={{ width: "25%", marginRight: "10px" }}
+              />
+            ))}
+          </div>
         </Form.Group>
         <Button
           variant="outline-success"
