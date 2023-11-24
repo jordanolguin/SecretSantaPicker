c8a23296c628c03ecb6566a17511950991c456ee
Jordan Olguin <jdolguin62@gmail.com>
2023-11-24 11:38:40 -0700

    updated game for real use

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,15 @@ import EnterPin from "./components/EnterPin";
 
 const App = () => {
   const participants = [
-    { id: 1, name: "John", pin: "1234" },
-    { id: 2, name: "Jane", pin: "5678" },
-    { id: 3, name: "Paul", pin: "9876" },
-    { id: 4, name: "Mary", pin: "4321" },
-    { id: 5, name: "Max", pin: "8765" },
-    { id: 6, name: "Sue", pin: "1357" },
+    { id: 1, name: "Tyler", pin: "1234" },
+    { id: 2, name: "Cat", pin: "5678" },
+    { id: 3, name: "Sam", pin: "9876" },
+    { id: 4, name: "Jordan", pin: "4321" },
+    { id: 5, name: "Nikki", pin: "8765" },
+    { id: 6, name: "Ashley", pin: "1357" },
   ];
 
-  const secretSantaAssignments = [
-    6, // John's secret Santa
-    5, // Jane's secret Santa
-    4, // Paul's secret Santa
-    1, // Mary's secret Santa
-    3, // Max's secret Santa
-    2, // Sue's secret Santa
-  ];
+  const secretSantaAssignments = [4, 3, 6, 5, 2, 1];
 
   return (
     <Router>
@@ -30,7 +23,12 @@ const App = () => {
         />
         <Route
           path="/enter-pin/:id"
-          element={<EnterPin participants={participants} secretSantaAssignments={secretSantaAssignments} />}
+          element={
+            <EnterPin
+              participants={participants}
+              secretSantaAssignments={secretSantaAssignments}
+            />
+          }
         />
       </Routes>
     </Router>
